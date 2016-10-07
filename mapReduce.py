a986b292ca43ce3aa0874fa32b09134929fda006
Félicien Catherin <felicien.catherin@gmail.com>
2016-10-07 14:42:17 +0200

    Reduce phase

--- mapReduce.py
+++ mapReduce.py
@@ -1,16 +1,17 @@
 # coding: utf8
 import sys
-import re 
+import re
 
 
 def map(text):
 	#Creation d'un dictionnaire où la clef est le mot et la valeur est une liste de 1, chaque un représentant une occurence du mot
 	list = []
+	#Séparation des mots
 	words = re.sub("[^\w]", " ",  text).split()
-	map = dict()
+	map = dict() #création du dictionnaire
 	for word in words:
 		if (word in map):
-            		map[word].append(1)
+            		map[word].append(1) 
         	else:
             		map[word] = [1]
     	list.append(map)
@@ -21,6 +22,15 @@ if(len(sys.argv) < 2):
 	print "Usage : "+sys.argv[0]+" FILE where FILE contains the words to count"
 	exit()
 
+def reduce(list):
+	dict = {}
+	for map in list:
+    		for key in map:
+        		if(key in finalDict):
+            			dict[key] = dict[key] + sumNumbersArray(map[key])
+        		else:
+            			dict[key] = sumNumbersArray(map[key])
+	return dict
 
 #Ouverture et lecture du fichier
 text = open(sys.argv[1], 'r').read()
@@ -31,3 +41,16 @@ text = re.sub('\W+',' ', text )
 
 #Map phase
 mapList = map(text)
+print mapList
+
+#Shuffle / Sort phase
+#SORT HERE
+
+#REDUCE phase
+dictionnary = reduce(mapList)
+
+
+#Affichage du résultat
+for key in dictionnary:
+	print key + " " + str(dictionnary[key])
+
